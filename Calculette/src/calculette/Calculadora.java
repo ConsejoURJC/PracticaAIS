5a9df1e0d45f0d67c9d1a4fcb827315b2140d651
g.ramosv <g.ramosv@TVD010060.dcloud.local>
2016-04-11 12:26:34 +0200

    Prueba

--- Calculette/src/calculette/Calculadora.java
+++ Calculette/src/calculette/Calculadora.java
@@ -15,6 +15,7 @@ public class Calculadora {
 
     public static void main(String[] args) {
         Interfaz a = new Interfaz();
+        Esto es codigo nuevo que debe crear conflicto
     }
     
 }
